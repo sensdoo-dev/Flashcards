53d7a924a48f036a8b51df07389bde13896d298e
Sensdoo <sensdoo.dev@gmail.com>
2025-01-23 18:31:37 +0300

    configurate database, generate models

---
+++ server/src/db/models/question.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Question extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Question.init({
+    question: DataTypes.STRING,
+    answer: DataTypes.STRING,
+    topic_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Question',
+  });
+  return Question;
+};
