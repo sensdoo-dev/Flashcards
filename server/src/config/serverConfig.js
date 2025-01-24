7bf0d4e843340b795dc9633f393ec2fc1e4f628d
Sensdoo <sensdoo.dev@gmail.com>
2025-01-24 11:40:13 +0300

    added middleware cors

--- server/src/config/serverConfig.js
+++ server/src/config/serverConfig.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const serverConfig = (app) => {
   app.use(morgan('dev'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
 };
 
 module.exports = serverConfig;
